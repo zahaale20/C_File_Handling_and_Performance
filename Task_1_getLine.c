e479fcf4aee4e1599699b2003790ee0e104cde15
zahaale20 <alexcz@outlook.com>
2023-10-16 19:04:10 -0700

    Argument handling

---
+++ Task_1_getLine.c
@@ -0,0 +1,14 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+    // Command line/argument error handling
+    if (argc != 2){
+        printf("Error: Incorrect argument usage. Ex: ./Task_1_getLine <filename>");
+        return 1;
+    }
+
+
+}

e4490f60fbcd8f172a337ff96eeeb51989448b6f
zahaale20 <alexcz@outlook.com>
2023-10-16 20:34:32 -0700

    Updated Task 2.

--- Task_1_getLine.c
+++ Task_1_getLine.c
@@ -6,13 +6,12 @@
 int main(int argc, char *argv[]){
     // Command line/argument error handling
     if (argc != 2){
-        printf("Error: Incorrect argument usage. Ex: ./Task_1_getLine <filename>\n");
+        printf("Error: Incorrect argument usage. Ex: ./a.out <filename>\n");
         return 1;
     }
 
     // Obtain file name & create file
-    const char *file_name = argv[1];
-    FILE *file = fopen(file_name, "r");
+    FILE *file = fopen(argv[1], "r");
 
     // File error handling
     if (file == NULL) {
--- Task_2_read.c
+++ Task_2_read.c
@@ -1,18 +1,14 @@
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <time.h>
 
 // Define size here (1, 2, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, and 8192)
-#define SIZE 8192
+#define SIZE 32
 
 int main(){
 
     // Create file
-    FILE *file = fopen("/usr/lib/locale/locale-archive", "rb"); // read binary mode
+    FILE *file = fopen("/usr/lib/locale/locale-archive", "rb"); // read file in binary mode
 
     // File error handling
     if (file == NULL) {
@@ -22,16 +18,16 @@ int main(){
 
     // Declare variables
     char buffer[SIZE];
-    size_t bytes_read;
-    clock_t start_time, end_time;
 
-    // Start time
-    start_time = clock();
-
-
-    // End time
-    end_time = clock();
+    while(fread(buffer, 1, SIZE, file) > 0){ // fread: https://www.tutorialspoint.com/c_standard_library/c_function_fread.htm
+        // No action, we aren't printing or doing anything here
+    }
 
     // Close file
     fclose(file);
 }
+
+// As sizes get smaller, the longer the program takes to run.
+// SIZE (of the buffer) determines how much data is in each chunk (in bytes). 
+/* Thus as we increase the size of the buffer, the data can be read in larger chunks 
+(as opposed to reading byte to byte, size of 1, for example) and the program will run more efficiently.*/

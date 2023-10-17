ff9f35d6c083c37ca4b8bb5da6001b55e3b69610
zahaale20 <alexcz@outlook.com>
2023-10-16 20:17:11 -0700

    Removing code and setting up the file

--- Task_2_read.c
+++ Task_2_read.c
@@ -9,16 +9,10 @@
 // Define size here (1, 2, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, and 8192)
 #define SIZE 8192
 
-int main(int argc, char *argv[]){
-    // Command line/argument error handling
-    if (argc != 2){
-        printf("Error: Incorrect argument usage. Ex: ./Task_1_getLine test.txt\n");
-        return 1;
-    }
+int main(){
 
-    // Obtain file name & create file
-    const char *file_name = argv[1];
-    FILE *file = fopen(file_name, "r");
+    // Create file
+    FILE *file = fopen("/usr/lib/locale/locale-archive", "rb"); // read binary mode
 
     // File error handling
     if (file == NULL) {
@@ -26,36 +20,18 @@ int main(int argc, char *argv[]){
         return 1;
     }
 
-    char *line = NULL;
-    size_t len = 0;
-    char *second_last_line = NULL;
-    char *last_line = NULL;
+    // Declare variables
+    char buffer[SIZE];
+    size_t bytes_read;
+    clock_t start_time, end_time;
 
-    // Begin reading lines
-    while (getline(&line, &len, file) != -1) {
-        printf("%s", line);
-        if (second_last_line != NULL){
-            free(second_last_line);
-        }
+    // Start time
+    start_time = clock();
 
-        second_last_line = last_line;
-        last_line = strdup(line);
-    }
 
-    // Print statements
-    printf("\n\n");
-    if (second_last_line != NULL){
-        printf("Second last line: %s", second_last_line);
-        free(second_last_line);
-    }
-    if (last_line != NULL){
-        printf("Last line: %s\n\n", last_line);
-        free(last_line);
-    }
+    // End time
+    end_time = clock();
 
     // Close file
     fclose(file);
-    
-    // Free memory allocation done by getLine
-    free(line);
 }

b17f99b3c6094679fb6380794ec312f3fc343e8c
zahaale20 <alexcz@outlook.com>
2023-10-16 19:34:33 -0700

    Updated code for finding the last two lines.

--- Task_1_getLine.c
+++ Task_1_getLine.c
@@ -6,7 +6,7 @@
 int main(int argc, char *argv[]){
     // Command line/argument error handling
     if (argc != 2){
-        printf("Error: Incorrect argument usage. Ex: ./Task_1_getLine <filename>");
+        printf("Error: Incorrect argument usage. Ex: ./Task_1_getLine <filename>\n");
         return 1;
     }
 
@@ -16,8 +16,32 @@ int main(int argc, char *argv[]){
 
     // File error handling
     if (file == NULL) {
-        printf("Error: Error retrieving and opening file name.")
+        printf("Error: Error retrieving and opening file name\n");
         return 1;
     }
 
+    char *line = NULL;
+    size_t len = 0;
+    char *second_last_line = NULL;
+    char *last_line = NULL;
+
+    // Begin reading lines
+    while (getline(&line, &len, file) != -1) {
+        printf("%s", line);
+        if (second_last_line != NULL){
+            free(second_last_line);
+        }
+
+        second_last_line = last_line;
+        last_line = strdup(line);
+    }
+    printf("\n\n");
+    printf("Second last line: %s", second_last_line);
+    printf("Last line: %s", last_line);
+
+    // Close file
+    fclose(file);
+    
+    // Free memory allocation done by getLine
+    free(line);
 }
